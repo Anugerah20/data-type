900742fbdb880c9b8facc076f942ea90b8f69a5a
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-04-04 11:26:15 +0700

    upload soal no 4

---
+++ soal4.js
@@ -0,0 +1,8 @@
+// No 4
+const roundUpToFive = (round) => {
+     return Math.ceil(round/5)*5;
+}
+
+console.log(roundUpToFive(32));
+console.log(roundUpToFive(137));
+console.log(roundUpToFive(142));
